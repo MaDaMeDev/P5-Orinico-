dd5331d6b35b6037f2e404dacb1e43faf7e52dcf
MaDaMeDeV <elodie.skotnicki@gmail.com>
2020-09-14 18:10:00 +0200

    creation du menu deroulant du choix des couleurs

--- Js/produits.js
+++ Js/produits.js
@@ -15,22 +15,54 @@ const callApi = async function () {
 
             function detailProduct(data) {
 
-                console.log(data)  
+                console.log(data) 
+                let title = document.createElement("h3")
+                title.classList.add("text-center")
+                document.querySelector(".title-prod").appendChild(title)
+                title.textContent = `${data.name}`
+ 
                 let article = document.createElement("article")
-                document.querySelector(".products").appendChild(article)
+                document.querySelector(".product").appendChild(article)
                 article.classList.add("card")
                 
                 let img = document.createElement("img")
                 img.classList.add("card-img-top")
                 img.setAttribute('src', `${data.imageUrl}`)
                 article.appendChild(img)
+
+                let desc = document.createElement("p")
+                article.appendChild(desc)
+                desc.textContent = `${data.description}`
+
+                let inputGroup = document.createElement("div")
+                article.appendChild(inputGroup)
+                inputGroup.classList.add("input-group")
+
+                let prepend = document.createElement("div")
+                inputGroup.appendChild(prepend)
+                prepend.classList.add("input-group-preprend")
+
+                let label = document.createElement("label")
+                prepend.appendChild(label)
+                label.classList.add("input-group-text")
+                label.textContent = "Couleur"
+
+                let select = document.createElement("select")
+                inputGroup.appendChild(select)
+                select.classList.add("custom-select")
+                select.setAttribute('id', "-colors")
                 
-                let title = document.createElement("h3")
-                article.appendChild(title)
-                title.classList.add("card-title")
-                title.textContent = `${data.name}`
+                let optionSelect = document.createElement("option", "selected")
+                select.appendChild(optionSelect)
+                optionSelect.textContent = "Choisir la couleur"
 
+                
+                data.colors.forEach((color) => {
+                    let option = document.createElement("option")
+                    select.appendChild(option)
+                    option.textContent = `${color}`
 
+                 });
             }
 
             detailProduct(data)
@@ -43,8 +75,6 @@ const callApi = async function () {
     }
 
     
-};
+}
 
 callApi() ;
-
-
